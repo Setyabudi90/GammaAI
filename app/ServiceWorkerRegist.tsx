db84a4d9a7e0e8485220364216a7010ccdd353d0
Inggrit Setya Budi <115447070+Setyabudi90@users.noreply.github.com>
2025-01-10 21:39:04 +0700

    Add files via upload

---
+++ app/ServiceWorkerRegist.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function ServiceWorkerRegistration() {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/gamma_service_worker.js")
+        .then((registration) => {
+          console.log("Service Worker successfully registered:", registration);
+        })
+        .catch((error) => {
+          console.error("Service Worker registration failed:", error);
+        });
+    }
+  }, []);
+
+  return null;
+}
