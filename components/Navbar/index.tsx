ba37fa6cb018f5a9702f969c1d10add3346e5a43
Inggrit Setya Budi <115447070+Setyabudi90@users.noreply.github.com>
2025-01-11 16:16:08 +0700

    Update index.tsx

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -3,7 +3,7 @@ import { Atom } from "lucide-react";
 
 export function Navbar() {
   return (
-    <header className="md:w-[85%] w-screen md:z-40 sm:z-0 flex justify-center md:justify-between dark:bg-slate-800 fixed bg-slate-100 items-center p-5 z-30 border-none shadow-none">
+    <header className="md:w-[85%] w-screen md:z-40 sm:z-0 flex justify-center md:justify-between dark:bg-[#212121] fixed bg-slate-100 items-center py-3 px-5 z-30 border-none shadow-none">
       <div className="font-extrabold text-2xl dark:text-[#bfaca8] md:translate-x-0 flex items-center gap-2 translate-x-5 text-slate-900">
         Gamma <Atom />
       </div>
